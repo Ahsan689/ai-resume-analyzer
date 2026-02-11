207a0296d6674be4991cdfb5bf237b156a6105be
ahsan689 <ahsanaleem689@gmail.com>
2026-02-11 19:01:26 +0500

    auth file created, puter script added in root

---
+++ app/routes/auth.tsx
@@ -0,0 +1,13 @@
+export const meta = () => ([
+    {title: 'Resumind | Auth'},
+    {name: 'description', content: 'Log into your account'},
+])
+import React from 'react'
+
+
+const Auth = () => {
+    return (
+        <div>Auth</div>
+    )
+}
+export default Auth
