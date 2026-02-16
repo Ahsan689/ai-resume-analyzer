4afda517a54ec279b8b682078b6b52cc2a8fddf2
ahsan689 <ahsanaleem689@gmail.com>
2026-02-16 17:12:08 +0500

    React Dropzone package installed

--- app/Components/FileUploader.tsx
+++ app/Components/FileUploader.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
 
 const FileUploader = () => {
+
+    const [file, setFile] = useState();
+
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
     return (
-        <div>FileUploader</div>
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+          <div className='space-y-4 cursor-pointer'></div>
+            <div className='mx-auto w-16 h-16 flex items-center justify-center'>
+                <img src='/icons/info.svg' alt='uplaod' className='size-20' />
+            </div>
+            {file ? (
+                <div>
+
+                </div>
+            ):(
+                <div>
+                    <p className='text-lg text-gray-500'>
+                        <span className='font-semibold'>
+                            Click to Upload
+                        </span>
+                    </p>
+                </div>
+            )
+            }
+        </div>
     )
 }
 export default FileUploader
